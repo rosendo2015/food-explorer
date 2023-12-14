e0744944db7370f1a8c8a982bb5afb1dafe18999
rosendo <rosendc30@gmail.com>
2023-12-13 21:28:26 -0300

    arquive routes

---
+++ api/src/routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const usersRoutes = require("./users.routes")
+const routes = Router()
+routes.use("/users", usersRoutes);
+module.exports = routes;
--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,7 @@
 const express = require("express");
+const routes = require("./routes");
 const app = express();
 app.use(express.json());
-
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body
-  response.json({ name, email, password })
-})
-
+app.use(routes);
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
