7cef12a1c6271284fd25a86dab04034fbe148910
rosendo <rosendc30@gmail.com>
2023-12-13 21:10:30 -0300

    method get teste

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body
+  response.json({ name, email, password })
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
