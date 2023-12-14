b3c607d0da40d82c82354177acba8d050b416f7d
rosendo <rosendc30@gmail.com>
2023-12-13 22:02:04 -0300

    tratamento de erro

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,7 +1,11 @@
+const AppError = require("../utils/AppError");
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
-    response.json({ name, email, password })
+    if (!name) {
+      throw new AppError("Nome é obrigatório.")
+    }
+    response.status(201).json({ name, email, password })
   }
 }
 module.exports = UsersController;
