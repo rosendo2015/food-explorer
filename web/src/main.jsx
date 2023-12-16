b14939aa7915f443a6f7f42bd1afaa3f6f5c0d6d
rosendo <rosendc30@gmail.com>
2023-12-16 08:33:10 -0300

    rotas para user autenticado

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from "./styles/global"
 import theme from './styles/theme'
-import { Menu } from './pages/Menu'
+import { Routes } from './routes'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Menu />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
