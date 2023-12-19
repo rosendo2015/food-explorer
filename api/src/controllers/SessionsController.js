ab012b35af71f52ad2729d30dd24d5bb9680e227
rosendo <rosendc30@gmail.com>
2023-12-18 22:30:12 -0300

    autenticação usando jwt

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,7 +1,26 @@
+const authConfig = require("../config/auth")
+const { sign } = require("jsonwebtoken")
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
-    return response.json({ email, password })
+    const user = await knex("users").where({ email }).first();
+    if (!user) {
+      throw new AppError("E-Mail e/ou senha incorreta", 401);
+    }
+    const passwordMatched = await compare(password, user.password);
+    if (!passwordMatched) {
+      throw new AppError("E-Mail e/ou senha incorreta", 401);
+    }
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+    return response.json({ user, token })
   }
 }
 module.exports = SessionsController;
