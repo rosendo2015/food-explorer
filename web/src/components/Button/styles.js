e5d526cfbbea88e1b1c9c2ab9aef5735ddbf6285
rosendo <rosendc30@gmail.com>
2023-12-20 23:52:56 -0300

    button

--- web/src/components/Button/styles.js
+++ web/src/components/Button/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ButtonStyle = styled.button`
 width: 100%;
-background: ${({ theme }) => theme.COLORS.TOMATO_400};
+background: ${({ theme }) => theme.COLORS.TOMATO_100};
 
 border-radius: 5px;
 border: 0;
