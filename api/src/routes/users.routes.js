61555da311b28561861d9dd8acfb60432924ddaf
rosendo <rosendc30@gmail.com>
2024-01-14 22:09:21 -0300

    upload - salvando endereço no bd

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -3,18 +3,17 @@ const multer = require("multer")
 const uploadConfig = require("../config/upload")
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const usersRoutes = Router();
-const upload = multer(uploadConfig.MULTER)
+const upload = multer(uploadConfig.MULTER);
 const usersController = new UsersController();
+const userAvatarController = new userAvatarController();
 
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"),(request,response)=>{
-    console.log(request.file.filename)
-    response.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"),userAvatarController.update)
 
 module.exports = usersRoutes;
